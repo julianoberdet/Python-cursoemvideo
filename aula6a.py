b22382862d84b56775095e712afbc6266c602cdd
julianoberdet <juliano_berdet@hotmail.com>
2025-09-10 11:03:07 -0300

    python

---
+++ aula6a.py
@@ -0,0 +1,10 @@
+# n1 = int(input('Digite um número: '))
+# print(type(n1))
+
+n1=int(input('Digite um número: '))
+n2=int(input('Digite outro número: '))
+n3=int(input('Digite outro número: '))
+s=n1+n2+n3
+# print('A soma entre' ,n1, ',' ,n2, 'e' ,n3, 'será',s) o de baixo é o mais recente
+print(f'A soma entre {n1} , {n2} e {n3} é {s}')
+
---
+++ aula6b.py
@@ -0,0 +1,19 @@
+# n = float(input('Digite um número: '))
+# print(n) se digitar 5, vai dar 5.0 por causa do float
+
+# n = input('Digite algo: ')
+# print(n.isnumeric()) se digitar qualquer número vai dar True
+
+# n = input('Digite algo: ')
+# print(n.isalpha()) se digitar qualquer palavra vai dar True
+
+# n = input('Digite algo: ')
+# print(n.isalnum()) se digitar número e letra junto da True
+
+
+
+
+
+
+
+
