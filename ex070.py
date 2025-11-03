3d1e0d6013435eefb1dc5548746584a4546bdb48
julianoberdet <juliano_berdet@hotmail.com>
2025-11-03 17:46:34 -0300

    finalizando módulo 2, condições e repetições

--- ex070.py
+++ ex070.py
@@ -1,13 +1,24 @@
-soma = mais = mais_barato = 0
+print('-' * 50)
+print('                  \033[34;1mLOJAS BERDET\033[m')
+print('-' * 50)
+total = tot1000 = menor = cont = 0
+barato = ''
 while True:
-    nome = str(input('Nome do produto: ')).strip()
+    produto = str(input('Nome do produto: ')).strip()
     preco = float(input('Preço do produto:R$ '))
-    usuario = str(input('Quer continuar:[S/N] ')).upper().strip()[0]
-    soma += preco
+    total += preco
+    cont += 1
     if preco > 1000:
-        mais += 1
-    if usuario == 'N':
+        tot1000 += 1
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = produto
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar?[S/N] ')).strip().upper()[0]
+    print('-' *50)
+    if resp == 'N':
         break
-print(f'O valor total dos produtos foi R${soma}')
-print(f'Temos {mais} produtos com valor acima de R$1000,00')
-print(f'O produto mais barato é o {mais_barato}')
+print(f'O total gasto na compra será: R${total:.2f}')
+print(f'Foram {tot1000} produtos acima de R$1000,00')
+print(f'O produto mais barato foi {barato} e custou R${menor:.2f}')
